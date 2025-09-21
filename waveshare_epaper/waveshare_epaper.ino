8b6cf91f11aaceca80e8813f0643061a9e6396f8
Mohan Gupta <mohangupta@live.com>
2025-09-21 15:00:40 +1000

    starting to write code to send raw spi data

--- waveshare_epaper/waveshare_epaper.ino
+++ waveshare_epaper/waveshare_epaper.ino
@@ -1,9 +1,50 @@
 #include <Arduino.h>
+#include <SPI.h>
 
+#define BUSY_PIN 4
+#define RESET_PIN 21
+#define DC_PIN 22
+
+// SPI.begin();
+// SPI.beginTransaction(SPISettings(2000000, MSBFIRST, SPI_MODE0));
 
 void setup() {
+    pinMode(SS, OUTPUT);
+    SPI.begin(SCK, -1, MOSI, SS);
+    pinMode(BUSY_PIN, INPUT);
+    pinMode(RESET_PIN, OUTPUT);
+    pinMode(DC_PIN, OUTPUT);
     
+    //hardware reset
+    digitalWrite(RESET_PIN, HIGH);
+    delayMicroseconds(10000);
+    digitalWrite(RESET_PIN, LOW);
+    delayMicroseconds(2000);
+    digitalWrite(RESET_PIN, HIGH);
+    delay(200);
+
+    //sendCommand(0x12) SWRESET
+    digitalWrite(DC_PIN, LOW);
+    delay(100);
+
+    //sendCommand
+
 }
 
-void loop() {
+void spiSend(uint8_t dcLevel, const uint8_t* buffer, size_t length) {
+  digitalWrite(DC_PIN, dcLevel);
+  digitalWrite(SS, LOW);
+
+  // Actually transmit
+  SPI.transfer((void*)buffer, length);
+
+  digitalWrite(SS, HIGH);
+}
+
+void send_command(const uint8_t* cmd, size_t length) {
+  spiSend(LOW, cmd, length);  // DC = 0 → command
+}
+
+void send_data(const uint8_t* data, size_t length) {
+  spiSend(HIGH, data, length); // DC = 1 → data
 }
